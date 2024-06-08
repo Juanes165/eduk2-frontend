cb4e407df67c8869a72abc3511835a7418f3b37d
KPOETA1 <108852892+KPOETA1@users.noreply.github.com>
2024-06-08 12:34:59 -0500

    feat: Update AchievementList component to use badge object properties
    The AchievementList component has been updated to use the properties of the badge object instead of accessing them through an array. This change improves readability and maintainability of the code.

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -15,108 +15,97 @@ import { Trophy, DeleteIcon } from "@/utils/icons/icons";
 
 export default function ProfilePage() {
 
-	// const { user } = useAuth();
-	// console.log(user);
-	const [showModalClose, setShowModalClose] = useState(false);
-	const userla = {
-		"badges": [["/images/avatar.svg", "Aragang", "Rompiendo los esquemas"], ["/images/avatar.svg", "Con Bendicion", "Siempre crei en ti"], ["/images/avatar.svg", "Soverano", "El rantantantantan"]],
-		"photoUrl": "/images/avatar.svg",
-		"role": "student",
-		"grade": "10-2",
-		"name": "Fuan",
-		"email": "tranhp@gmail.com",
-		"lastname": "Chostoy",
-		"points": 0
-	}
+    const { user } = useAuth();
+    console.log(user);
+    const [showModalClose, setShowModalClose] = useState(false)
 
-	const [role, setRole] = useState(null);
 
-	useEffect(() => {
-		if (userla.role === 'student') {
-			setRole('Estudiante');
-		} else if (userla.role === 'teacher') {
-			setRole('Profesor');
-		} else if (userla.role === 'admin') {
-			setRole('Administrador');
-		}
-	}, [userla.role]);
+    const [role, setRole] = useState(null);
 
-	const handleToggleClose = () => {
-		setShowModalClose(!showModalClose);
-	}
+    useEffect(() => {
+        if (user.role === 'student') {
+            setRole('Estudiante');
+        } else if (user.role === 'teacher') {
+            setRole('Profesor');
+        } else if (user.role === 'admin') {
+            setRole('Administrador');
+        }
+    }, [user.role]);
+
+    const handleToggleClose = () => {
+        setShowModalClose(!showModalClose);
+    }
 
 
 
 
-	return (
-		<>
-			<Header />
-			<div className="flex flex-col items-center justify-center h-full w-full mt-10">
-				<h1 className={`${pressStart2P.className} text-4xl mb-10`}>Perfil</h1>
-				<div className="flex w-4/5 h-4/5 flex-col md:flex-row">
-					<div className="w-full md:w-1/2">
-						{/* Contenido de la primera mitad de la tarjeta */}
-						{/* libro inclinado hoja izquierda */}
-						<div className="bg-white border border-gray-200 rounded-lg shadow-2xl dark:shadow-grape dark:bg-main-dark dark:border-gray-700 ">
-							<div className="flex justify-end px-4 pt-4">
-								{/* img onclick */}
-								<DeleteIcon alt="close" className="size-10 mb-4 rounded-full cursor-pointer" onClick={handleToggleClose} />
-							</div>
-							<div className="flex flex-col items-center pb-10">
-								<img src={userla.photoUrl} onError={(e) => e.target.src = '/images/avatar.svg'} alt="Logo" className="mb-4 rounded-full" />
-								<h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{userla.name} {userla.lastname}</h5>
-								<span className="text-sm text-gray-500 dark:text-gray-400">{role}</span>
-							</div>
-							<div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-								<dl className="sm:divide-y sm:divide-gray-200">
-									<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
-										<dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
-											Correo
-										</dt>
-										<dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
-											{userla.email}
-										</dd>
-									</div>
-									<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
-										<dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
-											Grado
-										</dt>
-										<dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
-											{userla.grade}
-										</dd>
-									</div>
-									<div className="dark:text-white py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
-										<dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
-											Puntos
-										</dt>
-										<dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
-											{userla.points}
-										</dd>
-									</div>
-								</dl>
-							</div>
-						</div>
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col items-center justify-center h-full w-full mt-10 mb-10">
+                <h1 className={`${pressStart2P.className} text-4xl mb-10 dark:border-b-2 shadow-md shadow-violet-dark dark:border-platinum dark:shadow-md dark:shadow-platinum  `}>Perfil</h1>
+                <div className="flex w-4/5 h-4/5 flex-col md:flex-row dark:shadow-2xl dark:shadow-platinum rounded">
+                    <div className="w-full md:w-1/2 mb-4 md:mb-0 dark:mb-4 dark:md:mb-0">
+                        {/* Contenido de la primera mitad de la tarjeta */}
+                        <div className="bg-white dark:bg-main-dark shadow-md shadow-violet-dark dark:border-gray-700 border-t-1 border-b-1 border-l-1 border-r-1 lg:rounded-l-lg rounded-t-lg sm:rounded-t-none border-gray-200 overflow-hidden">
 
-					</div>
-					<div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-white border border-gray-200 rounded-lg shadow-2xl dark:shadow-grape dark:bg-main-dark dark:border-gray-700">
-						{/* logros*/}
-						{/* <h2 className={`${pressStart2P.className} text-3xl mb-10`}>Logros</h2> */}
-						<div className="flex flex-wrap items-center justify-center ">
-								<p className={`${pressStart2P.className} text-3xl`}>Logros</p>
-								<Trophy className="size-10 ml-5" />
+                            <div className="flex flex-col items-center pb-10 mt-10">
+                                <img src={user.photoUrl} onError={(e) => e.target.src = '/images/avatar.svg'} alt="Logo" className="mb-4 rounded-full h-24 w-24" />
+                                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user.name} {user.lastname}</h5>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">{role}</span>
+                            </div>
+                            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                                <dl className="sm:divide-y sm:divide-gray-200 divide-y divide-gray-200">
+                                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
+                                        <dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
+                                            Correo
+                                        </dt>
+                                        <dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
+                                            {user.email}
+                                        </dd>
+                                    </div>
+                                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
+                                        <dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
+                                            Grado
+                                        </dt>
+                                        <dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
+                                            {user.grade}
+                                        </dd>
+                                    </div>
+                                    <div className="dark:text-white py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex flex-col items-center justify-center">
+                                        <dt className="dark:text-white text-sm font-medium text-gray-500 flex flex-col items-center justify-center">
+                                            Puntos
+                                        </dt>
+                                        <dd className="dark:text-white mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex flex-col items-center justify-center">
+                                            {user.points}
+                                        </dd>
+                                    </div>
+                                    <div className="flex justify-center px-4 pt-4">
+                                        <DeleteIcon alt="close" className="size-10 mb-4 rounded-full cursor-pointer" onClick={handleToggleClose} />
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                    </div>
 
-						</div>
-						<div className="flex flex-col">
-							<AchievementList badges={userla.badges} />
-						</div>
+                    
+                    <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-white shadow-md shadow-violet-dark dark:bg-main-dark dark:border-gray-700 border-t-1 border-b-1 border-r-1 lg:border-l-2 xl:border-l-2 border-gray-200 md:rounded-r-lg rounded-b-lg sm:rounded-b-none">
+                        {/* logros*/}
+                        <div className="flex flex-wrap items-center justify-center mb-4 mt-4">
+                            <p className={`${pressStart2P.className} text-3xl`}>Logros</p>
+                            <Trophy className="size-10 ml-5" />
+                        </div>
+                        <div>
+                            <AchievementList badges={user.badges} />
+                        </div>
 
-					</div>
-				</div>
-				{showModalClose &&
-					<div className="z-20 md:p-20 2xl:p-40 md:rounded-2xl absolute justify-center items-center self-center flex ">
-						<DeleteAccount closeModal={handleToggleClose} />
-					</div>
-				}
-			</div></>
-	);
+                    </div>
+                </div>
+                {showModalClose &&
+                    <div className="z-20 md:p-20 2xl:p-40 md:rounded-2xl absolute justify-center items-center self-center flex ">
+                        <DeleteAccount closeModal={handleToggleClose} />
+                    </div>
+                }
+            </div></>
+    );
 }
--- src/components/Achievement/AchievementList.jsx
+++ src/components/Achievement/AchievementList.jsx
@@ -1,24 +1,23 @@
-import Image from 'next/image';
-
-const AchievementList = ({ badges }) => {
+const AchievementList = ({ badges = [] }) => {
   return (
-    <ul role="list" className="divide-y divide-gray-400 transform">
+
+    <ul role="list" className="sm:divide-y sm:divide-gray-200 divide-y divide-gray-200">
       {badges.map((badge, index) => (
-        <li key={index} className="flex justify-between gap-x-40 md:gap-x-5 py-3">
-          <div className="flex min-w-0 gap-x-4">
-            <img className="h-12 w-12 flex-none rounded-full ml-4" src={badge[0]} alt={badge[1]} />
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-10 balance text-center text-gray-900 dark:text-white">{badge[1]}</p>
-              {/* <p className="mt-1 truncate text-xs leading-5 text-gray-500">{badge[2]}</p> */}
+        <li key={index} className=" justify-between gap-x-40 md:gap-x-5 py-3">
+          <div className="flex gap-x-4 items-center">
+            <img className="h-12 w-12 flex-none rounded-full ml-4" src={badge.url} alt={badge.title} />
+            <div className="text-left">
+              <p className="text-sm font-semibold leading-10 balance text-gray-900 dark:text-white">{badge.title}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{badge.description}</p>
             </div>
           </div>
           <div className="flex flex-col items-end ml-10">
-            <p className="text-sm font-semibold leading-10 text-gray-900 balance text-center mr-4 dark:text-white">{badge[2]}</p>
-            </div>
-
+            <p className="text-sm font-semibold leading-10 text-gray-900 balance text-center mr-4 dark:text-white">{badge.points}</p>
+          </div>
         </li>
       ))}
     </ul>
+
   );
 };
 
