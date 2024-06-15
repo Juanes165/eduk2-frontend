bbe43671032250a38e0e7c68920c2627186a2d7e
Esteban Betancourt <betancourtjuanes@gmail.com>
2024-06-14 23:42:03 -0500

    General refactor on layout, landing and access pages
    - Changed styles on landing page
    - Changed styles on access page
    - Fixed sidebar behaviour
    - Fixed layout and forum card

--- src/components/Nav/Sidebar.jsx
+++ src/components/Nav/Sidebar.jsx
@@ -1,6 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
+import { useState } from 'react';
 import { useAuth } from '@/hooks/useAuth';
 
 export default function Sidebar({ openedSidebar, sidebarRef }) {
@@ -10,30 +9,35 @@ export default function Sidebar({ openedSidebar, sidebarRef }) {
     const { user, courses } = useAuth();
 
     return (
-        <aside ref={sidebarRef} className={`group overflow-x-hidden border-r-[1px] border-main-dark/25 dark:border-main-light/25 bg-main-light dark:bg-main-dark z-50 overflow-auto flex flex-col justify-between ${openedSidebar ? "" : "-translate-x-64 lg:w-20 lg:translate-x-0"} px-4 py-4 absolute lg:sticky lg:top-16 w-64 transition-all duration-200 h-[calc(100dvh-4rem)] hover:w-64`}>
+        <aside
+            ref={sidebarRef}
+            className={`group overflow-x-hidden border-r-[1px] border-main-dark/25 dark:border-main-light/25 bg-main-light dark:bg-main-dark z-10 overflow-auto flex flex-col justify-between absolute
+                    ${openedSidebar ? "shadow-[5px_0px_5px_-5px_rgba(0,0,0,0.5)] lg:shadow-none" : "-translate-x-64 lg:w-20 lg:translate-x-0 hover:w-64 hover:delay-200 hover:shadow-[5px_0px_5px_-5px_rgba(0,0,0,0.5)]"} 
+                    px-4 py-4 lg:top-16 w-64 transition-all duration-200 h-[calc(100dvh-4rem)]`}
+        >
             {/* first half, profile picture, name and achievements */}
             <div className='mb-6'>
-                <div className={`${openedSidebar ? "px-8" : "px-8 lg:px-0"} mb-4 group-hover:px-8 group-hover:mb-4 text-center transition-all duration-200`}>
+                <div className={`${openedSidebar ? "px-8" : "px-8 lg:px-0 group-hover:delay-200"} mb-4 group-hover:px-8 group-hover:mb-4 text-center transition-all duration-200`}>
 
                     {/* Profile picture */}
                     <div className="relative">
-                        <img src={user.photoUrl} onError={(e) => e.target.src='/images/avatar.svg'} alt="Logo" className="mb-4 rounded-full bg-gray-500" />
-                        <Link href="/profile">
+                        <img src={user.photoUrl} onError={(e) => e.target.src = '/images/avatar.svg'} alt="Logo" className="mb-4 rounded-full bg-gray-500" />
+                        <a href="/profile">
                             <div className={`absolute top-0 right-0 ${openedSidebar ? "flex" : "hidden"} group-hover:flex w-full h-full rounded-full items-center justify-center bg-transparent cursor-pointer hover:bg-main-dark/25 text-main-light/0 hover:text-main-light transition-all duration-200`}>
                                 <span className='text-2xl font-semibold'>Ver perfil</span>
                             </div>
-                        </Link>
+                        </a>
                     </div>
 
                     {/* Name */}
-                    <span className={` ${openedSidebar ? "opacity-100 delay-100" : "opacity-0 delay-0"} group-hover:opacity-100 group-hover:delay-100 text-lg text-nowrap font-semibold`}>
+                    <span className={` ${openedSidebar ? "opacity-100 delay-100" : "opacity-0 delay-0"} group-hover:opacity-100 group-hover:delay-300 text-md text-nowrap font-semibold`}>
                         {user.name + " " + user.lastname}
                     </span>
                 </div>
 
 
                 {/* Achievements */}
-                <div className={`flex ${openedSidebar ? "px-4" : "px-0"} gap-2 flex-wrap group-hover:px-4 justify-between mb-4 transition-all duration-200`}>
+                <div className={`flex ${openedSidebar ? "px-4" : "px-0 group-hover:delay-200"} gap-2 flex-wrap group-hover:px-4 justify-between mb-4 transition-all duration-200`}>
                     <div className={`bg-amber-300 rounded-full h-12 w-12`} />
                     <div className={`bg-amber-300 rounded-full h-12 w-12`} />
                     <div className={`bg-amber-300 rounded-full h-12 w-12`} />
@@ -41,7 +45,7 @@ export default function Sidebar({ openedSidebar, sidebarRef }) {
             </div>
 
             {/* second half, courses navigation */}
-            <div>
+            <div className='h-fit'>
                 <div className='w-full border-t-[1px] border-main-dark/25 dark:border-main-light/25 mb-2'></div>
 
                 {/* deployable menu */}
@@ -49,31 +53,29 @@ export default function Sidebar({ openedSidebar, sidebarRef }) {
                     <svg className="mr-6 fill-main-dark dark:fill-main-light" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M7.5 22q-1.45 0-2.475-1.025T4 18.5v-13q0-1.45 1.025-2.475T7.5 2H20v15q-.625 0-1.062.438T18.5 18.5q0 .625.438 1.063T20 20v2zM6 15.325q.35-.175.725-.25T7.5 15H8V4h-.5q-.625 0-1.062.438T6 5.5zM10 15h8V4h-8zm-4 .325V4zM7.5 20h9.325q-.15-.35-.237-.712T16.5 18.5q0-.4.075-.775t.25-.725H7.5q-.65 0-1.075.438T6 18.5q0 .65.425 1.075T7.5 20" />
                     </svg>
-                    <span className={`group-hover:block text-nowrap text-lg`}>
+                    <span className={`group-hover:block text-nowrap text-md`}>
                         Tus cursos
                     </span>
-                    <svg className={`ml-8 animate-all duration-300 ${showNav ? "-rotate-90" : "rotate-90"} fill-main-dark dark:fill-main-light`} width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <svg className={`ml-8 animate-all duration-500 ${showNav ? "-rotate-90" : "rotate-90"} fill-main-dark dark:fill-main-light`} width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M8.025 22L6.25 20.225L14.475 12L6.25 3.775L8.025 2l10 10z" />
                     </svg>
                 </button>
 
                 {/* courses */}
-                {showNav && (
-                    <nav className={`delay-200 ${openedSidebar ? "flex" : ""} group-hover:flex group-hover:visible`}>
-                        <ul className='w-full'>
-                            {courses.map((course, index) => (
-                                <li className='mt-2' key={index}>
-                                    <Link href={'/course/' + course.id} className='flex items-center w-full rounded-xl border-main-dark/25 dark:border-main-light/50 px-3 py-1 text-lg hover:bg-platinum dark:hover:bg-violet-dark'>
-                                        <div className='flex'>
-                                            <img src={course.urlPhoto} alt="Logo" className="mr-5 w-8 h-8 rounded-full" />
-                                            <span className='align-middle'>{course.name}</span>
-                                        </div>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                )}
+                <nav className={`${openedSidebar ? "flex " : "group-hover:flex group-hover:visible"} ${showNav ? "max-h-[300vh] visible" : "max-h-0 invisible overflow-hidden"} transition-all duration-500`}>
+                    <ul className='w-full'>
+                        {courses.map((course, index) => (
+                            <li className='mt-2' key={index}>
+                                <a href={'/course/' + course.id} className='flex items-center w-full rounded-xl border-main-dark/25 dark:border-main-light/50 px-3 py-1 text-md hover:bg-platinum dark:hover:bg-violet-dark'>
+                                    <div className='flex max-w-[200px]'>
+                                        <img src={course.urlPhoto} alt="Logo" className="mr-5 w-8 h-8 rounded-full" />
+                                        <span className='align-middle truncate'>{course.name}</span>
+                                    </div>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
             </div>
         </aside>
     );
--- src/utils/icons/icons.js
+++ src/utils/icons/icons.js
@@ -6,24 +6,6 @@ export const VideoIcon = (props) => {
     )
 }
 
-// export const ImageIcon = () => {
-//     return (
-//         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="#ffffff">
-//             <path d="M0 0h24v24H0z" fill="none" />
-//             <path d="M20 18V6c0-1.1-.9-2-2-2H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2zM6 18l3-4 2.25 3 3-4.5L18 18H6z" />
-//         </svg>
-//     )
-// }
-
-// export const PdfIcon = () => {
-//     return (
-//         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="#ffffff">
-//             <path d="M0 0h24v24H0z" fill="none" />
-//             <path d="M16 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-4-6zm-2 15H8v-2h6v2zm0-4H8v-2h6v2zm0-4H8V7h6v2zm2-5.66L18.07 7H16V2.34zM13 13V4.08L17.92 9H13z" />
-//         </svg>
-//     )
-// }
-
 export const DeleteIcon = (props) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}>
@@ -93,3 +75,9 @@ export function PdfIcon(props) {
         </svg>
     )
 }
+
+export function LoadingIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2"><path strokeDasharray="60" strokeDashoffset="60" strokeOpacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"/></path><path strokeDasharray="15" strokeDashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></g></svg>
+    )
+  }
